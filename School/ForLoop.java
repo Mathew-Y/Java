88e7f160fcf22567631bf7cf1d8231cc34fed5a9
Mathew-Y <47284187+Mathew-Y@users.noreply.github.com>
2022-06-23 21:26:56 -0400

    Delete School directory

--- School/ForLoop.java
+++
@@ -1,9 +0,0 @@
-public class ForLoop {
-
-    public static void main(String[] args) {
-
-        for(int i = 1; i <= 25; i = i + 1) {
-            System.out.println(i);
-        }
-    }
-}
--- School/Interfaces/info.java
+++
@@ -1,6 +0,0 @@
-package Interfaces;
-
-public interface info {
-
-    public void showInfo();
-}
